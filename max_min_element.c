0c9f6c1a758a9e6862cb5ca182c68b619abe6d07
Arpan Pandey <pandeyarpan2006@gmail.com>
2026-03-19 22:19:16 +0530

    Add files via upload

---
+++ max_min_element.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    int i;
+
+    printf("Enter size of array : ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enter array elements : ");
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    int max = a[0];
+    int min = a[0];
+
+    for(i = 1; i < n; i++) {
+
+        if(a[i] > max) {
+            max = a[i];
+        }
+
+        if(a[i] < min) {
+            min = a[i];
+        }
+    }
+
+    printf("Max: %d\n", max);
+    printf("Min: %d", min);
+
+    return 0;
+}
