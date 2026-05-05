321594f57bdc631a8f86b9ff4cec38c5daf8542f
Arpan Pandey <pandeyarpan2006@gmail.com>
2026-05-05 13:37:27 +0530

    Day 54

---
+++ zigzag_transversal.c
@@ -0,0 +1,112 @@
+/*
+Problem: Zigzag (Spiral) Level Order Traversal
+
+Input:
+- First line: integer N
+- Second line: level-order traversal (-1 for NULL)
+
+Output:
+- Print zigzag traversal
+
+Example:
+7
+1 2 3 4 5 6 7
+
+Output:
+1 3 2 4 5 6 7
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    int arr[100];
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* nodes[100];
+
+    for(int i = 0; i < n; i++) {
+
+        if(arr[i] == -1)
+            nodes[i] = NULL;
+        else {
+            nodes[i] = (struct Node*)malloc(sizeof(struct Node));
+            nodes[i]->data = arr[i];
+            nodes[i]->left = NULL;
+            nodes[i]->right = NULL;
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+
+        if(nodes[i] != NULL) {
+
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+
+            if(left < n)
+                nodes[i]->left = nodes[left];
+
+            if(right < n)
+                nodes[i]->right = nodes[right];
+        }
+    }
+
+    struct Node* root = nodes[0];
+
+    struct Node* queue[100];
+    int front = 0, rear = 0;
+
+    if(root != NULL)
+        queue[rear++] = root;
+
+    int level = 0;
+
+    while(front < rear) {
+
+        int size = rear - front;
+        int temp[100];
+        int idx = 0;
+
+        for(int i = 0; i < size; i++) {
+
+            struct Node* curr = queue[front++];
+            temp[idx++] = curr->data;
+
+            if(curr->left != NULL)
+                queue[rear++] = curr->left;
+
+            if(curr->right != NULL)
+                queue[rear++] = curr->right;
+        }
+
+        if(level % 2 == 0) {
+            for(int i = 0; i < idx; i++) {
+                printf("%d", temp[i]);
+                if(!(front >= rear && i == idx - 1)) printf(" ");
+            }
+        }
+        else {
+            for(int i = idx - 1; i >= 0; i--) {
+                printf("%d", temp[i]);
+                if(!(front >= rear && i == 0)) printf(" ");
+            }
+        }
+
+        level++;
+    }
+
+    return 0;
+}
